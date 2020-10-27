5d689f57c328cc56c585f15028b03027d642b379
RomanTsibii <romanot70@gmail.com>
2020-10-27 12:52:21 +0200

    start rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,10 @@ class Task < ApplicationRecord
   belongs_to :flow_step
 
   enum level: {
-      trainee: 0,
-      junior: 1,
-      middle: 5,
-      senior: 10
+    trainee: 0,
+    junior: 1,
+    middle: 5,
+    senior: 10
   }
 
   has_rich_text :description
