dfcf56307b2b7f4e2456b6e7ee1119bf36f03589
RomanTsibii <romanot70@gmail.com>
2020-10-30 13:48:58 +0200

    Remane host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { :host => 'roman-bootcamp-2020.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'glacial-anchorage-80923.herokuapp.com/' }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
