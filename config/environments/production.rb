4cd06450c58ed7b4723743f248b0fb33c631a5d0
RomanTsibii <romanot70@gmail.com>
2020-10-30 14:16:45 +0200

    Config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'glacial-anchorage-80923.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'glacial-anchorage-80923.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 25,
