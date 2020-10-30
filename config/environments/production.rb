c36b5baf2468baa6079a686b31849f9e3e507133
RomanTsibii <romanot70@gmail.com>
2020-10-30 14:06:51 +0200

    Config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,17 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { :host => 'glacial-anchorage-80923.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'glacial-anchorage-80923.herokuapp.com/' }
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 25,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['SENDGRID_DOMAIN'],
+    authentication: :plain
+  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
